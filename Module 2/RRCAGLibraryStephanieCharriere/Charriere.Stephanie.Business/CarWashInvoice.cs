9cb76f4a3321693d20f2e15d761b18d05c9f8bc6
Stephanie Charriere <63465084+scharriere14@users.noreply.github.com>
2025-02-24 22:42:48 -0600

    Financial class (#1)
    * Created CarWashInvoice. Has 5 errors.
    
    * Made Financial class. One error.

---
+++ Module 2/RRCAGLibraryStephanieCharriere/Charriere.Stephanie.Business/CarWashInvoice.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Charriere.Stephanie.Business
+{
+    /// <summary>
+    /// This class contains functionality that supports the business process of creating an invoice for the car wash department.
+    /// The CarWashInvoice class derives from the Invoice class.
+    /// </summary>
+    class CarWashInvoice : Invoice
+    {
+        // The amount charged for the chosen package.
+        private decimal packageCost;
+
+        // The amount charged for the chosen fragrance.
+        private decimal fragranceCost;
+
+        /// <summary>
+        /// Gets and sets the amount charged for the chosen package.
+        /// </summary>
+        /// <param name="value">The value of the Package Cost</param>"
+        /// <exception cref=" ArgumentOutOfRangeException">Thrown when the property is set to less than or greater than 0.</exception>
+        public decimal PackageCost
+        {
+            get { return packageCost; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("The value cannot be less than 0.");
+                }
+                else
+                {
+                    this.packageCost = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets and sets the amount charged for the chosen fragrance.
+        /// </summary>
+        /// ArgumentOutOfRangeException - Thrown when the property is set to less than 0. Message: “The value cannot be less than 0.” Parameter name: “value”.
+
+        public decimal FragranceCost
+        {
+            get { return fragranceCost; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("The value cannot be less than 0.");
+                }
+                else
+                {
+                    this.fragranceCost = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the amount of provincial sales tax charged to the customer. No provincial sales tax is charged for a car wash.
+        /// </summary>
+        public decimal ProvincialSalesTaxCharged { get; }
+
+        /// <summary>
+        /// decimal - Gets the amount of goods and services tax charged to the customer (Rounded to 2 decimal places).
+        /// The tax is calculated by multiplying the tax rate by the subtotal.
+        /// </summary>
+        public decimal GoodsAndServicesTaxCharged { get; }
+
+        /// <summary>
+        /// Gets the subtotal of the Invoice. The subtotal is the sum of the package and fragrance cost.
+        /// </summary>
+        public decimal SubTotal { get; }
+
+        /// <summary>
+        /// Initializes an instance of CarWashInvoice with a provincial and goods and services tax rates.
+        /// The package cost and fragrance cost are zero.
+        /// </summary>
+        /// <param name="provincialSalesTaxRate"> The rate of provincial tax charged to a customer.</param>
+        /// <param name="goodsAndServicesTaxRate"> The rate of goods and services tax charged to a customer.</param>
+        /// <exception cref=" ArgumentOutOfRangeException">Thrown when </exception>
+        // ArgumentOutOfRangeException - Thrown when the provincial sales tax rate is less than 0. Message: “The argument cannot be less than 0.” Parameter name: “provincialSalesTaxRate”.
+        //        ArgumentOutOfRangeException - Thrown when the provincial sales tax rate is greater than 1. Message: “The argument cannot be greater than 1.” Parameter name: “provincialSalesTaxRate”.
+        //ArgumentOutOfRangeException - Thrown when the goods and services tax rate is less than 0. Message: “The argument cannot be less than 0.” Parameter name: “goodsAndServicesTaxRate”.
+        //ArgumentOutOfRangeException - Thrown when the goods and services tax rate is greater than 1. Message: “The argument cannot be greater than 1.” Parameter name: “goodsAndServicesTaxRate”.
+        public CarWashInvoice(decimal provincialSalesTaxRate, decimal goodsAndServicesTaxRate)
+        {
+            PackageCost = 0;
+            FragranceCost = 0;
+
+            if (provincialSalesTaxRate < 0)
+            {
+                throw new ArgumentOutOfRangeException("The argument cannot be less than 0.");
+            }
+            if (provincialSalesTaxRate > 1)
+            {
+                throw new ArgumentOutOfRangeException("The argument cannot be greater than 1.");
+            }
+
+            this.ProvincialSalesTaxRate = provincialSalesTaxRate;
+
+            if (goodsAndServicesTaxRate < 0)
+            {
+                throw new ArgumentOutOfRangeException("The argument cannot be less than 0.");
+            }
+            if (goodsAndServicesTaxRate > 1)
+            {
+                throw new ArgumentOutOfRangeException("The argument cannot be greater than 1.");
+            }
+
+            this.GoodsAndServicesTaxRate = goodsAndServicesTaxRate;
+       
+        }
+
+        /// <summary>
+        /// Initializes an instance of CarWashInvoice with a provincial and goods, services tax rate, package cost and fragrance cost.
+        /// </summary>
+        /// <param name="provincialSalesTaxRate"></param>
+        /// <param name="goodsAndServicesTaxRate"></param>
+        /// <param name="packageCost"></param>
+        /// <param name="fragranceCost"></param>
+        ///
+
+        // Initializes an instance of CarWashInvoice with a provincial and goods, services tax rate, package cost and fragrance cost.
+        //        Parameters:
+        //provincialSalesTaxRate - The rate of provincial tax charged to a customer.
+        //goodsAndServicesTaxRate - The rate of goods and services tax charged to a customer.
+        //packageCost - The cost of the chosen package.
+        //fragranceCost - The cost of the chosen fragrance.
+
+        // Exceptions:
+        // ArgumentOutOfRangeException - Thrown when the provincial sales tax rate is less than 0. Message: “The argument cannot be less than 0.” Parameter name: “provincialSalesTaxRate”.
+        //ArgumentOutOfRangeException - Thrown when the provincial sales tax rate is greater than 1. Message: “The argument cannot be greater than 1.” Parameter name: “provincialSalesTaxRate”.
+        //ArgumentOutOfRangeException - Thrown when the goods and services tax rate is less than 0. Message: “The argument cannot be less than 0.” Parameter name: “goodsAndServicesTaxRate”.
+                //ArgumentOutOfRangeException - Thrown when the goods and services tax rate is greater than 1. Message: “The argument cannot be greater than 1.” Parameter name: “goodsAndServicesTaxRate”.
+        //ArgumentOutOfRangeException - Thrown when the package cost is less than 0. Message: “The argument cannot be less than 0.” Parameter name: “packageCost”.
+        //ArgumentOutOfRangeException - Thrown when the fragrance cost is less than 0. Message: “The argument cannot be less than 0.” Parameter name: “fragranceCost”.
+        public CarWashInvoice(
+            decimal provincialSalesTaxRate,
+            decimal goodsAndServicesTaxRate,
+            decimal packageCost,
+            decimal fragranceCost
+        )
+        {
+            if (provincialSalesTaxRate < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(provincialSalesTaxRate), "The argument cannot be less than 0.");
+            }
+
+            if (provincialSalesTaxRate > 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(provincialSalesTaxRate), "The argument cannot be greater than 1.");
+            }
+
+            this.ProvincialSalesTaxRate = provincialSalesTaxRate;
+
+            if (goodsAndServicesTaxRate < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(goodsAndServicesTaxRate), "The argument cannot be less than 0.");
+            }
+
+            if (goodsAndServicesTaxRate > 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(goodsAndServicesTaxRate), "The argument cannot be greater than 1.");
+            }
+
+            this.GoodsAndServicesTaxRate = goodsAndServicesTaxRate;
+
+            if (packageCost < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(packageCost), "The argument cannot be less than 0.");
+            }
+            this.PackageCost = packageCost;
+            if (fragranceCost < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(fragranceCost), "The argument cannot be less than 0.");
+            }
+            this.FragranceCost = fragranceCost;
+        }
+    }
+}
---
+++ Module 2/RRCAGLibraryStephanieCharriere/Charriere.Stephanie.Business/Financial.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Charriere.Stephanie.Business
+{
+    /// <summary>
+    /// //This static class contains functionality that includes financial functions.
+    /// </summary>
+    static class Financial
+    {
+        /// <summary>
+        /// Returns the payment amount for an annuity based on periodic, fixed payments and a fixed interest rate.
+        /// </summary>
+        /// <param name="rate">the interest rate per period.For example, if the rate is an annual percentage rate(APR) of 10 percent and the customer makes monthly payments, the rate per period is 0.1 / 12, or 0.0083.</param>
+        /// <param name="numberOfPaymentPeriods">            numberOfPaymentPeriods - the total number of payment periods in the annuity.For example, if you make monthly payments on a four - year car loan, your loan has a total of 4 × 12(or 48) payment periods.</param>
+        /// <param name="presentValue">            presentValue - the present value(or lump sum) that a series of payments to be paid in the future is worth now. For example, when a customer finances a car, the loan amount is the present value to the lender of the car payments the customer will make.</param>
+        /// <returns></returns>
+        public decimal GetPayment(decimal rate, int numberOfPaymentPeriods, decimal presentValue)
+        {
+            //Exceptions:
+            //ArgumentOutOfRangeException - Thrown when the rate is less than 0.Message: “The argument cannot be less than 0.” Parameter name: “rate”.
+            if (rate < 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    "The argument cannot be less than 0.",
+                    "rate"
+                );
+            }
+            //ArgumentOutOfRangeException - Thrown when the rate is greater than 1.Message: “The argument cannot be greater than 1.” Parameter name: “rate”.
+            if (rate > 1)
+            {
+                throw new ArgumentOutOfRangeException(
+                    "The argument cannot be greater than 1.",
+                    "rate"
+                );
+            }
+            //ArgumentOutOfRangeException - Thrown when the number of payments is less than or equal to zero.Message: “The argument cannot be less than or equal to 0.” Parameter name: “numberOfPaymentPeriods”.
+            if (numberOfPaymentPeriods <= 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    "The argument cannot be less than or equal to 0.",
+                    "numberOfPaymentPeriods"
+                );
+            }
+            //ArgumentOutOfRangeException - Thrown when the present value is less than or equal to zero.Message: “The argument cannot be less than or equal to 0.” Parameter name: “presentValue”.
+            if (rate <= 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    "The argument cannot be less than or equal to 0.",
+                    "presentValue"
+                );
+            }
+
+            //The implementation for the GetPayment() method is:
+            decimal futureValue = 0;
+            decimal type = 0;
+            decimal payment = 0;
+            if (rate == 0)
+                payment = presentValue / numberOfPaymentPeriods;
+            else
+                payment =
+                    rate
+                    * (
+                        futureValue
+                        + presentValue
+                            * (decimal)Math.Pow((double)(1 + rate), (double)numberOfPaymentPeriods)
+                    )
+                    / (
+                        ((decimal)Math.Pow((double)(1 + rate), (double)numberOfPaymentPeriods) - 1)
+                        * (1 + rate * type)
+                    );
+            return Math.Round(payment, 2);
+        }
+    }
+}
